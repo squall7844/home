b3d99c37a93611c23b2f9670c8d9f42aea4a10f4
squall7844 <mizukitsujimoto09@gmail.com>
2024-02-13 22:10:36 +0900

    commit!!!!!

--- src/app/components/Bus/MiniPlayer.tsx
+++ src/app/components/Bus/MiniPlayer.tsx
@@ -6,14 +6,14 @@ export default function MiniPlyer() {
       <Image
         className="rounded-3xl"
         src="/背景.jpg"
-        width={260}
-        height={250}
+        width={360}
+        height={290}
         alt="ミニプレイヤー背景"
       ></Image>
       <iframe
         className="absolute flex justify-center items-center"
-        width="200"
-        height="140"
+        width="290"
+        height="190"
         src="https://www.youtube.com/embed/playlist?list=PLAba57iCK96m2_jMuL9BDpRqup2Ilnail"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
