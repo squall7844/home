fa40252ead9f7155a5d516f2177c808ff58f363f
squall7844 <mizukitsujimoto09@gmail.com>
2024-02-20 00:05:57 +0900

    一時的にログインなし

--- src/app/pages/bus/page.tsx
+++ src/app/pages/bus/page.tsx
@@ -9,24 +9,36 @@ import BusTime from "@/components/Bus/BusTime";
 import MiniPlyer from "@/components/Bus/MiniPlayer";
 import TopButton from "@/components/TopButton";
 
+// export default function Bus() {
+//   const [user] = useAuthState(auth);
+//   const router = useRouter();
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     if (user) {
+//       setLoading(false);
+//     } else {
+//       router.push("auth/login");
+//     }
+//   }, [user, router]);
+
+//   if (loading) {
+//     return;
+//     <div>Loading...</div>;
+//   }
+
+//   return (
+//     <div className="bg-gradient-to-b from-gray-800 to-blue-950 text-white">
+//       <Header />
+//       <Response />
+//       <TopButton />
+//       <MiniPlyer />
+//       <BusTime />
+//     </div>
+//   );
+// }
+
 export default function Bus() {
-  const [user] = useAuthState(auth);
-  const router = useRouter();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    if (user) {
-      setLoading(false);
-    } else {
-      router.push("auth/login");
-    }
-  }, [user, router]);
-
-  if (loading) {
-    return;
-    <div>Loading...</div>;
-  }
-
   return (
     <div className="bg-gradient-to-b from-gray-800 to-blue-950 text-white">
       <Header />
